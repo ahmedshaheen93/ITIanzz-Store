33169567a5c93ef16a91f957b319032896247e2f
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-04-09 14:29:50 +0200

    removed some noise

--- src/main/java/eg/gov/iti/jets/config/ConnectionToDB.java
+++ src/main/java/eg/gov/iti/jets/config/ConnectionToDB.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eg.gov.iti.jets.config;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.Objects;
 
-/**
- * @author lts
- */
 public class ConnectionToDB {
 
     private static ConnectionToDB instance;
